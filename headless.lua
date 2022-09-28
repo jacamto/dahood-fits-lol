f656d508fc77529de54de1fa0ea274996fdb720d
jacamto <111714642+jacamto@users.noreply.github.com>
2022-09-27 23:50:57 -0400

    Delete headless.lua

--- headless.lua
+++
@@ -1,3 +0,0 @@
-game.Players.istolewillshoodie.Character.Head.Transparency = 1
-game.Players.istolewillshoodie.Character.Head.face:Destroy()
-game.Players.istolewillshoodie.Character.Head.face:Destroy()
